510668e81b0747b6a34f268dd687c9430fd62af9
LinhT-6104 <binbicute1997@gmail.com>
2023-10-23 13:19:21 +0700

    update

---
+++ Python/Test/Python/check.py
@@ -0,0 +1,8 @@
+Name = 'Python'
+a,b,c,d,e,f = Name
+print(a)    
+print(b)    
+print(c)    
+print(d)    
+print(e)    
+print(f)    
--- Python/code mẫu trường/Test/Python/check.py
+++
@@ -1,8 +0,0 @@
-Name = 'Python'
-a,b,c,d,e,f = Name
-print(a)    
-print(b)    
-print(c)    
-print(d)    
-print(e)    
-print(f)    
