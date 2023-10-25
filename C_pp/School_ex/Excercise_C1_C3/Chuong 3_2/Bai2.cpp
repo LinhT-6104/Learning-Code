5b3ab7445d44616148b2338cc894bf9d1526bf42
LinhT-6104 <97030072+LinhT-6104@users.noreply.github.com>
2023-10-25 09:39:53 +0700

    Add files via upload

---
+++ C_pp/School_ex/Excercise_C1_C3/Chuong 3_2/Bai2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "Nhap chuoi: ";
+	string s;
+	getline(cin,s);
+	
+	// Dem so luong tu trong chuoi
+	int count = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i-1] == ' ' or i == 0) count++;
+	}
+	cout << "So luong tu trong chuoi: " << count << endl;
+	
+	// Capitalize Each Word
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i-1] == ' ' or i == 0) {
+			s[i] = toupper(s[i]);
+		} else {
+			s[i] = tolower(s[i]);
+		}
+	}
+	cout << "Capitalize Each Word: " << s;
+	return 0;
+}
