9077d583dcc86d5af4a52442cd81974d3c2d5d2d
LinhT-6104 <binbicute1997@gmail.com>
2023-12-16 10:51:46 +0700

    Ontapcst

---
+++ C_pp/School_ex/On_tap/CST/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    string a = "1234";
+    cout << stoi(a) + 2;
+
+    return 0;
+}
--- C_pp/School_ex/Tuan 11/test.cpp
+++ C_pp/School_ex/Tuan 11/test.cpp
@@ -1,141 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-class TDate {
+class complex {
 private:
-    short day;
-    short month;
-    short year;
+    string name;
+    int age;
 
 public:
-    TDate() : day(0), month(0), year(0) {}
-
-    void nhapDL() {
-        cin >> day >> month >> year;
+    complex() {
+        name = "";
+        age = 0;
     }
-
-    void hienThi() {
-        cout << day << "/" << month << "/" << year;
+    void setName(string n) {
+        name = n;
     }
-
-    int khoangCach() {
-        int result = (year - 1) * 365 + day;
-        for (int i = 1; i < month; i++) {
-            switch (i) {
-                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    result += 31;
-                    break;
-                case 4: case 6: case 9: case 11:
-                    result += 30;
-                    break;
-                case 2:
-                    if ((year % 4 == 0 and year % 100 != 0) or (year % 100 == 0 and year % 400 == 0)) {
-                        result += 29;
-                    }
-                    else {
-                        result += 28;
-                    }
-                    break;
-            }
-        }
-
-        return result;
+    string getName() {
+        return name;
     }
 };
-
-class Person {
-private:
-    string hoTen;
-    TDate ngaySinh;
-    string queQuan;
-
-public:
-    Person() {}
-
-    Person(string hoTen, TDate ngaySinh, string queQuan)
-        : hoTen(hoTen), ngaySinh(ngaySinh), queQuan(queQuan) {}
-
-    Person(const Person &a)
-        : hoTen(a.hoTen), ngaySinh(a.ngaySinh), queQuan(a.queQuan) {}
-
-    void nhapDL() {
-        cout << "Nhap ho ten: ";
-        getline(cin, hoTen);
-
-        cout << "Nhap ngay sinh: ";
-        ngaySinh.nhapDL();
-
-        cout << "Nhap que quan: ";
-        getline(cin, queQuan);
-    }
-
-    void hienThi() {
-        cout << "Ho ten: " << hoTen << endl;
-        cout << "Ngay sinh: ";
-        ngaySinh.hienThi();
-        cout << endl;
-        cout << "Que quan: " << queQuan << endl;
-    }
-
-    int layTuoi() {
-        return ngaySinh.khoangCach() / 365;
-    }
-
-    string layHoTen() const {
-        return hoTen;
-    }
-};
-
-bool sapXepTheoTuoi(Person &nguoi1, Person &nguoi2) {
-    return nguoi1.layTuoi() < nguoi2.layTuoi();
-}
-
-void inNguoiCoHoTenAnh(const vector<Person> &danhSach) {
-    cout << "Nhung nguoi co ho ten chua tu 'Anh':\n";
-    for (const Person &nguoi : danhSach) {
-        if (nguoi.layHoTen().find("Anh") != string::npos) {
-            nguoi.hienThi();
-            cout << endl;
-        }
-    }
-}
 
 int main() {
-    int n;
-    do {
-        cout << "Nhap so nguoi (n < 50): ";
-        cin >> n;
-        cin.ignore();
-    } while (n >= 50);
-
-    vector<Person> danhSachNguoi(n);
-
-    for (int i = 0; i < n; i++) {
-        cout << "Nhap thong tin cho nguoi thu " << i + 1 << ":\n";
-        danhSachNguoi[i].nhapDL();
-    }
-
-    cout << "\nDanh sach nguoi vua nhap:\n";
-    for (int i = 0; i < n; i++) {
-        cout << "Nguoi thu " << i + 1 << ":\n";
-        danhSachNguoi[i].hienThi();
-        cout << endl;
-    }
-
-    sort(danhSachNguoi.begin(), danhSachNguoi.end(), sapXepTheoTuoi);
-
-    cout << "\nDanh sach nguoi theo tuoi tang dan:\n";
-    for (int i = 0; i < n; i++) {
-        cout << "Nguoi thu " << i + 1 << ":\n";
-        danhSachNguoi[i].hienThi();
-        cout << endl;
-    }
-
-    inNguoiCoHoTenAnh(danhSachNguoi);
-
+    complex A;
+    A.setName("Hong Anh");
+    cout << A.getName();
     return 0;
 }
